8062895c13c1214a05546edb0aa8ed3678071d89
Liam Ball <davidliamb38@gmail.com>
2023-10-03 16:17:31 -0600

    why???

--- app/services/ApplesService.js
+++ app/services/ApplesService.js
@@ -8,7 +8,11 @@ class ApplesService {
   buyApple(apple) {
     if (AppState.Money >= apple.price) {
       AppState.Money -= apple.price
-      Pop.success(`You Bought a ${apple.name}`)
+      if (apple.name == 'Green Apple') {
+        Pop.toast('why????', "question")
+      } else {
+        Pop.success(`You Bought a ${apple.name}`)
+      }
     } else {
       Pop.error(`You don't have $${apple.price} for a ${apple.name}`)
     }
